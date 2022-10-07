f7d005b8107fb285c03909256e0ffdbb5c20a0d1
naomi-amelia <110778119+naomi-amelia@users.noreply.github.com>
2022-10-07 16:51:19 +0700

    Add files via upload

---
+++ deploy.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import pandas as pd
+import tensorflow as tf
+import pickle
+
+preprocess = pickle.load(open('pipe.pkl', 'rb'))
+model = tf.keras.models.load_model('churn_model.h5')
+
+st.header('Telco Customer Churn Predictor')
+
+st.write('Please enter the information below:')
+tenure = st.number_input('Customer tenure duration')
+
+SeniorCitizen = st.selectbox('Senior citizenship status', ['No', 'Yes'])
+PaymentMethod = st.selectbox('Subcription payment method', ['Electronic check', 'Mailed check', 'Bank transfer (automatic)', 'Credit card (automatic)'])
+PaperlessBilling = st.selectbox('paperless billing', ['Yes', 'No'])
+Contract = st.selectbox('Subscription contract terms', ['Month-to-month', 'One year', 'Two year'])
+Partner = st.selectbox('Customer partner present', ['Yes', 'No'])
+Dependents = st.selectbox('Customer dependents present', ['Yes', 'No'])
+TechSupport = st.selectbox('Tech support service', ['Yes', 'No', 'No internet service'])
+OnlineSecurity = st.selectbox('Online security service', ['Yes', 'No', 'No internet service'])
+OnlineBackup = st.selectbox('Online backup service', ['Yes', 'No', 'No internet service'])
+DeviceProtection = st.selectbox('Device protection service', ['Yes', 'No', 'No internet service'])
+StreamingTV = st.selectbox('TV streaming service', ['Yes', 'No', 'No internet service'])
+StreamingMovies = st.selectbox('Movies streaming service', ['Yes', 'No', 'No internet service'])
+InternetService = st.selectbox('Internet service', ['DSL', 'Fiber optic', 'No internet service'])
+MultipleLines = st.selectbox('Multiple lines', ['Yes', 'No', 'No phone service'])
+
+
+if st.button('submit'):
+    
+    num_cols = ['tenure']
+    cat_cols = ['SeniorCitizen', 'Partner', 'Dependents', 'MultipleLines', 'InternetService',
+               'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport',
+               'StreamingTV', 'StreamingMovies', 'Contract', 'PaperlessBilling',
+               'PaymentMethod']
+
+    num_df = pd.DataFrame([[tenure]], columns=num_cols)
+
+    cat_df = pd.DataFrame([[SeniorCitizen,Partner, Dependents, MultipleLines, InternetService,
+               OnlineSecurity, OnlineBackup, DeviceProtection, TechSupport,
+               StreamingTV, StreamingMovies, Contract, PaperlessBilling,
+               PaymentMethod]], columns=cat_cols)
+
+    x = pd.concat([num_df, cat_df], axis=1)
+
+    xs = pd.DataFrame(preprocess.transform(x))
+
+    pred = model.predict(xs)
+
+   
+    if pred[0][0] < 0.5:
+      st.write('Not churn')
+    else:
+      st.write('Churn')
